fc4c5dde5082bd7aca00aa9e4d9b59267ce711fc
Lilach E <lilachelayza@gmail.com>
2025-02-10 10:30:49 +0200

    add GET /polls

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import { json } from "body-parser";
 import { getConnection, initConnection } from "./dbConnection";
+import { FieldPacket, RowDataPacket } from "mysql2";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -12,6 +13,20 @@ app.use(json());
 
 app.get("/check", (_, res) => {
   res.status(200).json({ message: "Hello" });
+});
+
+app.get("/polls", async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const query = `SELECT * FROM weVote.polls;`;
+
+    const [polls]: [RowDataPacket[], FieldPacket[]] = await connection.promise().query(query);
+
+    res.status(200).json(polls);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Couldn't fetch polls from database." });
+  }
 });
 
 async function init() {
