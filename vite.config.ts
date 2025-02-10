2c2a2ae54dc35e654dd621d07673bdded357c983
Lilach E <lilachelayza@gmail.com>
2025-02-10 10:30:05 +0200

    add tailwindcss 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,9 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
