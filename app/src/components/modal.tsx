e9f392cf8783cf57808ff4dcc6ef0c2672f4f3cc
Lilach E <lilachelayza@gmail.com>
2025-02-10 13:55:13 +0200

    connect form to POST /poll endpoint

--- app/src/components/modal.tsx
+++ app/src/components/modal.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react"
+import { server } from "../services/axiosInstance";
 
 interface NewPoll {
     title: string;
@@ -25,8 +26,16 @@ export function Modal({ isVisible, onClose }: ModalProps) {
         setNewPoll((prevPoll) => ({ ...prevPoll, [name]: value, }));
     }
 
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        try {
+            await server.post("/poll", newPoll);
+            console.log("Poll created successfully!", newPoll);
+            onCancel();
+        } catch (error) {
+            console.error("Failed to submit form", error);
+        }
     }
 
     return (
@@ -35,13 +44,13 @@ export function Modal({ isVisible, onClose }: ModalProps) {
                 <h2>Create a new Poll</h2>
                 <form className="flex flex-col items-center gap-4 pt-2" onSubmit={onSubmit}>
                     <input name="title" onChange={handleChange} className="w-full placeholder-gray-400 placeholder-opacity-50 text-black bg-white p-1 rounded" value={newPoll.title} type="text" placeholder="Poll Title" />
-                    <input name="deadline" onChange={handleChange} className="w-full placeholder-gray-400 placeholder-opacity-50 text-black bg-white p-1 rounded" value={newPoll.deadline} type="text" placeholder="Deadline" />
+                    <input name="deadline" onChange={handleChange} className="w-full placeholder-gray-400 placeholder-opacity-50 text-black bg-white p-1 rounded" value={newPoll.deadline} type="date" placeholder="Deadline" />
                     <input name="cost" onChange={handleChange} className="w-full placeholder-gray-400 placeholder-opacity-50 text-black bg-white p-1 rounded" value={newPoll.cost} type="text" placeholder="Cost" />
                     <input name="details" onChange={handleChange} className="w-full placeholder-gray-400 placeholder-opacity-50 text-black bg-white p-1 rounded" value={newPoll.details} type="text" placeholder="Details" />
 
                     <div className="w-full flex justify-between">
                         <button className="w-max py-1 px-4 rounded" type="submit">Create Poll</button>
-                        <button className="w-max py-1 px-4 rounded destructive" onClick={onCancel}>Cancel</button>
+                        <button className="w-max py-1 px-4 rounded destructive" onClick={onCancel} type="button">Cancel</button>
                     </div>
                 </form>
             </div>
