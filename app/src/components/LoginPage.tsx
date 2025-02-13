43ec7565f4b027bd81d40d4737e0fdeea03c409e
Lilach E <lilachelayza@gmail.com>
2025-02-13 11:10:49 +0200

    add LoginPage component

---
+++ app/src/components/LoginPage.tsx
@@ -0,0 +1,9 @@
+import "./LoginPage.css";
+
+export function LoginPage() {
+    return (
+        <>
+        <h1 className="lilach">Login</h1>
+        </>
+    )
+}
--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { PollPage } from "./components/PollPage";
 import { NotFoundPage } from "./components/NotFoundPage";
+import { LoginPage } from "./components/LoginPage";
 
 export const router = createBrowserRouter([
     { path: "/", element: <App /> },
+    { path: "/login", element: <LoginPage /> },
     { path: "/polls/:id", element: <PollPage /> },
     { path: "*", element: <NotFoundPage /> },
 ]);
