fc94d67fb183fb17842497f963fffc1c6d64e4ad
Lilach E <lilachelayza@gmail.com>
2025-02-18 12:51:34 +0200

    add login form

--- app/src/components/LoginPage.tsx
+++ app/src/components/LoginPage.tsx
@@ -1,7 +1,73 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+const formSchema = z.object({
+  phone: z.string().regex(/^\d{10}$/, "Phone number must be exactly 10 digits"),
+});
+
 export function LoginPage() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      phone: "",
+    },
+  });
+
+  function onSubmit(value: z.infer<typeof formSchema>) {
+    console.log(value);
+  }
+
   return (
     <>
-      <h1>Login</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-3xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Enter your phone number below to login
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Phone number" {...field} />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="mx-auto w-full">
+                Login
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </>
   );
 }
