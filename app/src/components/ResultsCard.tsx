77a616036f4396ec21f44f904d94a9ac00753571
Lilach E <lilachelayza@gmail.com>
2025-03-03 10:05:32 +0200

    create ResultsCard component

---
+++ app/src/components/ResultsCard.tsx
@@ -0,0 +1,32 @@
+import { Poll } from "@/types/poll";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { ResultsPie } from "./ResultsPie";
+
+type ResultsCardProps = {
+  pollStatus: string;
+  poll: Poll;
+};
+
+export function ResultsCard({ pollStatus, poll }: ResultsCardProps) {
+  return (
+    <Card className="w-1/2">
+      <CardHeader className="text-center pb-0">
+        <CardTitle>
+          <h2>Results</h2>
+        </CardTitle>
+        <CardDescription>
+          Will display here after the poll is closed
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {pollStatus === "Closed" ? <ResultsPie poll={poll} /> : null}
+      </CardContent>
+    </Card>
+  );
+}
