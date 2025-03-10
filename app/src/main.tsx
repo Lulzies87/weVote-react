755ccef159f778aaa63beaf6ab4e416e3cdc18d7
Lilach E <lilachelayza@gmail.com>
2025-03-10 16:19:08 +0200

    add HashRouter

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter, RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { TenantProvider } from "./context/TenantContext";
 import "./index.css";
@@ -8,7 +8,9 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TenantProvider>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <RouterProvider router={router} />
+      </HashRouter>
     </TenantProvider>
   </StrictMode>
 );
