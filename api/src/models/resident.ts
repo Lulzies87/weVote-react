28602ba162db15e570089c391e987447085d8ec5
Lilach E <lilachelayza@gmail.com>
2025-04-05 10:36:06 +0300

    add Resident model

---
+++ api/src/models/resident.ts
@@ -0,0 +1,10 @@
+export interface Resident {
+  id: number;
+  fName: string;
+  lName: string;
+  tenantID: string;
+  email: string;
+  phone: string;
+  apartment: number;
+  createdAt: string;
+}
